beafb2ddd829c2e1eea86057ca2bf5db296c6716
jadedeterville <jadedeterville@gmail.com>
2021-11-21 22:05:25 +0000

    Nav updates

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const  hamburgerEl = document.getElementsByClassName("hamburger")[0];
 
 const  navListEl = document.getElementsByClassName("nav-list")[0];
 
-
-
 hamburgerEl.addEventListener("click", () =>{
-  navListEl.classList.toggle('active')
+  navListEl.classList.toggle("active")
 })
