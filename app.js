e7ad602b5c4868fa0939c62cba8babe631614e56
jadedeterville <jadedeterville@gmail.com>
2021-11-21 18:34:23 +0000

    Added JS Hamburger

---
+++ app.js
@@ -0,0 +1,9 @@
+const  hamburgerEl = document.getElementsByClassName("hamburger")[0];
+
+const  navListEl = document.getElementsByClassName("nav-list")[0];
+
+
+
+hamburgerEl.addEventListener("click", () =>{
+  navListEl.classList.toggle('active')
+})
